e96b61eaae3c302b9574420496b71e5f21c4aefc
dateatharva11 <dateatharva@gmail.com>
2026-03-30 09:05:45 -0700

    Increasing payload capacity in server's index file

--- server/index.ts
+++ server/index.ts
@@ -14,13 +14,14 @@ declare module "http" {
 
 app.use(
   express.json({
+    limit: '10mb', 
     verify: (req, _res, buf) => {
       req.rawBody = buf;
     },
   }),
 );
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false, limit: "10mb" }));
 
 export function log(message: string, source = "express") {
   const formattedTime = new Date().toLocaleTimeString("en-US", {
@@ -52,7 +53,7 @@ app.use((req, res, next) => {
         logLine += ` :: ${JSON.stringify(capturedJsonResponse)}`;
       }
 
-      log(logLine);
+      // log(logLine);
     }
   });
 
@@ -90,10 +91,12 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
+  // const port = 5000;
+  // const host = "127.0.0.1"
   httpServer.listen(
     {
       port,
-      host: "0.0.0.0",
+      // host,
       reusePort: true,
     },
     () => {
